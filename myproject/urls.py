ab298d0e4e8a2348036bc6c065970c06a2c60593
N.C.H 145 <hungnc1267@ut.edu.vn>
2025-11-28 23:41:13 +0700

    Delete myproject directory

--- myproject/urls.py
+++
@@ -1,8 +0,0 @@
-# myproject/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('translation.urls')),  # trỏ tới urls của app translation
-]
