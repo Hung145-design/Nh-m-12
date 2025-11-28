609e748bf26ce7257d291d7b0476fdc3434865c5
N.C.H 145 <hungnc1267@ut.edu.vn>
2025-11-28 23:41:21 +0700

    Delete translation directory

--- translation/admin.py
+++
@@ -1,8 +0,0 @@
-# Register your models here.
-from django.contrib import admin
-from .models import TranslationHistory
-
-@admin.register(TranslationHistory)
-class TranslationHistoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "source_text", "translated_text", "target_lang", "created_at")
-    search_fields = ("source_text", "translated_text")
--- translation/urls.py
+++
@@ -1,7 +0,0 @@
-# translation/urls.py
-from django.urls import path
-from .views import TranslateView
-
-urlpatterns = [
-    path('translate/', TranslateView.as_view(), name='translate'),
-]
